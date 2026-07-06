9692c60f4ee22dc53419d57adff6b13a9be23912
Varshil Soni <sonivarshil000@gmail.com>
2026-07-06 13:08:21 +0530

    Added Question 1

--- app.py
+++ app.py
@@ -74,6 +74,12 @@ def signup():
         return redirect(url_for("login"))
 
     return render_template("signup.html")
+@app.route("/logout")
+def logout():
+
+    session.pop("user",None)
+
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(debug=True)
